c4d28f2879f80aa9c0c4c2aec7407fd944d78466
Alice Moretti <ali.moret88@gmail.com>
2022-10-20 11:50:49 +0300

    Added data to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">test</div>;
+  const dummyMovies = [
+    {
+      id: 1,
+      title: "Some Dummy Movie",
+      openingText: "This is the opening text of the movie",
+      releaseDate: "2021-05-18",
+    },
+    {
+      id: 2,
+      title: "Some Dummy Movie 2",
+      openingText: "This is the second opening text of the movie",
+      releaseDate: "2021-05-19",
+    },
+  ];
+
+  return (
+    <>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+    </>
+  );
 }
 
 export default App;
