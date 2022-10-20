9744ebf536744c9377e1ac22bc5f49050aad788d
Alice Moretti <ali.moret88@gmail.com>
2022-10-20 14:13:12 +0300

    Replaced .then with async/await

--- src/App.js
+++ src/App.js
@@ -4,23 +4,28 @@ import { useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  function fetchMovies() {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_data,
-          };
-        });
-        setMovies(transformedMovies);
+  async function fetchMovies() {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch("https://swapi.dev/api/films");
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_data,
+        };
       });
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    } catch (error) {}
   }
 
   return (
@@ -29,7 +34,9 @@ function App() {
         <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>There are no movies</p>}
+        {isLoading && <p>Loading movies...</p>}
       </section>
     </>
   );
